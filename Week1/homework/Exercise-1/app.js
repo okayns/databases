1d28833c5e8df9a548a6dcd3eac143c077b59f8d
okayns <55080187+okayns@users.noreply.github.com>
2020-05-12 08:44:55 +0200

    2nd commit according to feedbacks...

--- Week1/homework/Exercise-1/app.js
+++ Week1/homework/Exercise-1/app.js
@@ -9,7 +9,7 @@ const connection = mysql.createConnection({
 const tables = [
   "CREATE TABLE Invitee (invitee_no int, invitee_name varchar(30), invited_by varchar(30))",
   "CREATE TABLE Room (room_no int, room_name varchar(30), floor_number int)",
-  "CREATE TABLE Meeting (meeting_no int, meeting_title varchar(30), starting_time timestamp, ending_time timestamp, room_no varchar(5))",
+  "CREATE TABLE Meeting (meeting_no int, meeting_title varchar(30), starting_time timestamp, ending_time timestamp, room_no INT)",
 ];
 
 const insert_queries = [
@@ -23,14 +23,18 @@ const insert_queries = [
   "INSERT INTO Room VALUES (338, 'Collective IQ Room', 3)",
   "INSERT INTO Room VALUES (313, 'Inspiration Station', 3)",
   "INSERT INTO Room VALUES (513, 'Hospitality Hub', 5)",
-  "INSERT INTO Meeting VALUES (1, 'Building for the Future', '2020-11-03 12:00', '2020-12-03 12:00', 'a2')",
-  "INSERT INTO Meeting VALUES (2, 'Customer Focus', '2020-11-03 12:00', '2020-11-04 12:00', 'b12')",
-  "INSERT INTO Meeting VALUES (3, 'Do Great Things', '2020-01-23 08:00', '2020-01-24 12:00', 'c3')",
-  "INSERT INTO Meeting VALUES (4, 'Igniting Team Spirit', '2020-08-13 12:00', '2020-11-13 12:00', 'd1')",
-  "INSERT INTO Meeting VALUES (5, 'Anything is Possible', '2020-03-14 12:00', '2020-04-13 12:00', 'a7')",
+  "INSERT INTO Meeting VALUES (1, 'Building for the Future', '2020-11-03 12:00', '2020-12-03 12:00', '1')",
+  "INSERT INTO Meeting VALUES (2, 'Customer Focus', '2020-11-03 12:00', '2020-11-04 12:00', '2')",
+  "INSERT INTO Meeting VALUES (3, 'Do Great Things', '2020-01-23 08:00', '2020-01-24 12:00', '3')",
+  "INSERT INTO Meeting VALUES (4, 'Igniting Team Spirit', '2020-08-13 12:00', '2020-11-13 12:00', '1')",
+  "INSERT INTO Meeting VALUES (5, 'Anything is Possible', '2020-03-14 12:00', '2020-04-13 12:00', '2')",
 ];
 
 connection.connect();
+
+connection.query("DROP DATABASE IF EXISTS meetup", function (err, result) {
+  if (err) throw err;
+});
 
 connection.query("CREATE DATABASE meetup", function (err, result) {
   if (err) throw err;
